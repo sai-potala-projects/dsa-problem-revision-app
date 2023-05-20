2ebd948b47807e52293fcf82f4053171d4d03912
bhaskar <bhaskarpotala@gmail.com>
2023-05-20 15:20:15 +0530

    random problem picker route added

--- Backend/routers/dsaProblemRouter.ts
+++ Backend/routers/dsaProblemRouter.ts
@@ -54,7 +54,6 @@ dsaProblemRouter.post(
 
     const result = await Promise.all(updatePromises);
     const allProblems = await getLatestProblemsList(userObjectId);
-
     res.status(202).send({ problems: allProblems });
   })
 );
@@ -66,6 +65,74 @@ dsaProblemRouter.post(
     const { _id: userObjectId } = req.userInfo;
     const allProblems = await getLatestProblemsList(userObjectId);
     res.status(202).send({ problems: allProblems });
+  })
+);
+
+dsaProblemRouter.post(
+  '/delete',
+  isAuth,
+  expressAsyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+    const { problemIds } = req.body;
+    const { _id: userObjectId } = req.userInfo;
+
+    await Problem.deleteMany({ _id: { $in: problemIds } });
+
+    // Delete problems from UserProblemList collection
+    await UserProblemList.updateMany({}, { $pull: { problems: { $in: problemIds } } });
+    const allProblems = await getLatestProblemsList(userObjectId);
+    res.status(202).send({ problems: allProblems });
+  })
+);
+
+dsaProblemRouter.post(
+  '/picker',
+  isAuth,
+  expressAsyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+    const { _id: userObjectId } = req.userInfo;
+    const { difficultyLevel } = req.body;
+
+    // Fetch the user's problem list
+    const userProblemList: any = await getLatestProblemsList(userObjectId);
+
+    //filter is completed ones
+    const finalProblemList: any = userProblemList.filter((problem: any) => !problem.isCompleted);
+
+    if (!userProblemList?.length) {
+      res.status(202).send({ error: 'no problems found' });
+    }
+    // Filter the problems based on your criteria (e.g., unsolved, difficulty level)
+    const unsolvedProblems = finalProblemList.filter((problem: any) => !problem.isSolved);
+    if (unsolvedProblems.length > 0) {
+      let filteredProblems;
+
+      if (difficultyLevel && difficultyLevel !== '') {
+        // If the desired difficulty level is provided, prioritize unsolved problems from that level
+        filteredProblems = unsolvedProblems.filter((problem: any) => problem.difficultyLevel === difficultyLevel);
+      } else {
+        // If no difficulty level is specified, select randomly from all difficulty levels
+        const difficultyLevels = ['Easy', 'Medium', 'Hard'];
+        const selectedDifficultyLevel = difficultyLevels[Math.floor(Math.random() * difficultyLevels.length)];
+        filteredProblems = unsolvedProblems.filter(
+          (problem: any) => problem.difficultyLevel === selectedDifficultyLevel
+        );
+      }
+      // Retrieve the first problem from the filtered list (problem picker)
+      const problemPicker = filteredProblems[0];
+
+      res.status(200).send(problemPicker);
+    } else {
+      // If all problems are solved, prioritize the oldest solved problem
+      const sortedProblems = finalProblemList.sort((a: any, b: any) => {
+        // Sort based on last time solved (ascending)
+        const dateA = new Date(a.lastTimeSolved);
+        const dateB = new Date(b.lastTimeSolved);
+        return dateA.getTime() - dateB.getTime();
+      });
+
+      // Retrieve the first problem from the sorted list (problem picker)
+      const problemPicker = sortedProblems[0];
+      res.status(200).send(problemPicker);
+    }
   })
 );
 
